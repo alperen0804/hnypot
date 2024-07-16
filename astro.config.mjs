39035f1267103e3d0b0dddc7ea266afbfed698a6
Alperen Yilmaz <alperen.developer@icloud.com>
2024-07-16 23:37:10 +0200

    Add site domain to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://hnypot.dev',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
