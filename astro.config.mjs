ff65618eeb6b1d922c8c91573c9b7d2eca16cfe5
Alperen Yilmaz <alperen.developer@icloud.com>
2024-07-16 13:23:27 +0200

    Integrate Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
-
 import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: node({
     mode: "standalone"
