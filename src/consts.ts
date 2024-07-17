2dba5896bda87b109c899564c02b11fc6a6c78cc
Alperen Yilmaz <alperen.developer@icloud.com>
2024-07-17 21:09:35 +0200

    Edit SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'hnypot';
-export const SITE_DESCRIPTION = 'This Is My Portfolio.';
+export const SITE_DESCRIPTION = 'This Is My Portfolio!';
